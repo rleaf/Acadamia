310792ef2bb035cad7d62705c4d7dce6115c65fe
ryan <calcifer44@gmail.com>
2020-02-04 15:20:52 -0500

    120 class 2

---
+++ 220/_02/src/Address.java
@@ -0,0 +1,15 @@
+// Ryan Lin, CSC 120, 2/4/20
+//
+
+public class Address {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ryan Lin");
+        System.out.println("2083 Lawrenceville Road");
+        System.out.println("Lawrenceville NJ 08648");
+        System.out.println("973 555 1234");
+
+    }
+
+}
---
+++ 220/_02/src/StarPattern.java
@@ -0,0 +1,20 @@
+public class StarPattern {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("********");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
+
+//        System.out.print("*\n");
+//        System.out.print("**\n");
+//        System.out.print("***\n");
+
+    }
+
+}
