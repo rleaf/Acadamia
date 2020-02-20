866478e5400ce958ed94f6b2d8d6428fadf693b9
ryan <calcifer44@gmail.com>
2020-02-20 15:43:18 -0500

    CSC 220 2_20

--- 220/2_20/EmployeeDemo.java
+++ 220/2_20/EmployeeDemo.java
@@ -5,18 +5,21 @@ public class EmployeeDemo {
 
    public static void main(String[] args) {
       
+      //  Instantiating objects
       Employee testOne = new Employee();
       Employee testTwo = new Employee();
       Employee testThree = new Employee();
 
+      // Setting Object Attributes
       testOne.set("MD Ali", 12345, "Computer Science");
       testTwo.set("Sean Kane", 54321, "Information Technologoy");
       testThree.set("Nasir Jones", 13579, "Business");
 
+      // Getting Object Attributes
       System.out.printf("%s %s %d \n", testOne.getName(), testOne.getDepartment(), testOne.getId());
       System.out.printf("%s %s %d \n", testTwo.getName(), testTwo.getDepartment(), testTwo.getId());
       System.out.printf("%s %s %d \n", testThree.getName(), testThree.getDepartment(), testThree.getId());
-      
+
 
    }
 
