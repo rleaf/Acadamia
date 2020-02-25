53460400cd2c44a550e4eb94abddcaa670ea58ca
ryan <calcifer44@gmail.com>
2020-02-25 15:45:44 -0500

    2_25

---
+++ 220/2_25/t.java
@@ -0,0 +1,11 @@
+public class t {
+
+   public static void main(String[] args) {
+
+      tClass abc = new tClass(5, 6);
+      
+      System.out.println(abc.x);
+
+   }
+
+}
---
+++ 220/2_25/tClass.java
@@ -0,0 +1,12 @@
+public class tClass {
+
+   public int x;
+   public int y;
+
+
+   public tClass(int _x, int _y) {
+      _x = x;
+      _y = y;
+   }
+
+}
