3e0c05032f87ef729965bf811fbb7699ff989a36
ryan <calcifer44@gmail.com>
2020-03-03 15:06:49 -0500

    switch 3_3

---
+++ 220/3_3/pChallenge.java
@@ -0,0 +1,43 @@
+public class pChallenge {
+
+   public static void main(String[] args) {
+
+      // int choice = 2;
+
+      switch (choice) {
+         case 1:
+            System.out.println("You selected 1.");
+            break;
+         case 2:
+         case 3:
+            System.out.println("You selected 2 or 3.");
+            break;
+         case 4:
+            System.out.println("You selected 4.");
+            break;
+
+         default:
+            System.out.println("Select again please.");
+            break;
+      }
+
+   }
+
+}
+
+// if (choice == 1)
+// {
+// System.out.println("You selected 1.");
+// }
+// else if (choice == 2 || choice == 3)
+// {
+// System.out.println("You selected 2 or 3.");
+// }
+// else if (choice == 4)
+// {
+// System.out.println("You selected 4.");
+// }
+// else
+// {
+// System.out.println("Select again please.");
+// }
