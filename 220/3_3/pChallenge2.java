0c219d6fee6117278e66c580cd2830396c9b95da
ryan <calcifer44@gmail.com>
2020-03-03 15:34:15 -0500

    120

---
+++ 220/3_3/pChallenge2.java
@@ -0,0 +1,26 @@
+public class pChallenge2 {
+
+   public static void main(String[] args) {
+
+      int choice = 1;
+
+      switch (choice) {
+
+         case 1:
+            System.out.println("toads");
+            break;
+
+         case 2:
+            System.out.println("banana");
+            break;
+
+         default:
+            System.out.println("pancake");
+            break;
+
+      }
+
+   }
+
+
+}
