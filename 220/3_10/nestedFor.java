29b398f8c1ad116aa018e53606213538e8808448
ryan <calcifer44@gmail.com>
2020-03-12 14:46:50 -0400

    fill

--- 220/3_10/nestedFor.java
+++ 220/3_10/nestedFor.java
@@ -6,13 +6,7 @@ public class nestedFor {
       int x = 0;
 
       for (int i = 1; i < 3; i++) {
-
-         // System.out.println("test " + i);
-
          for (int j = 1; j < 3; j++) {
-
-            // System.out.println("toaders " + j);
-
             x = i * j;
             System.out.println(x);
 
---
+++ 220/3_10/test.java
@@ -0,0 +1,15 @@
+public class test {
+
+   public static void main(String[] args) {
+      
+      int x = 0;
+
+      for (int count = 10; count <= 21; count++) {
+         // System.out.println(count);
+         System.out.println(x++);
+
+      }
+
+   }
+
+}
