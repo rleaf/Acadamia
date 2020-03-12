12d36f05f5e58e0bcd1afd09b6ce2e546408a36f
ryan <calcifer44@gmail.com>
2020-03-12 16:09:03 -0400

    3_12 csc 120

--- 220/3_10/test.java
+++ 220/3_10/test.java
@@ -2,13 +2,32 @@ public class test {
 
    public static void main(String[] args) {
       
-      int x = 0;
+      // int x = 0;
 
-      for (int count = 10; count <= 21; count++) {
-         // System.out.println(count);
-         System.out.println(x++);
+      // for (int count = 10; count <= 21; count++) {
+      //    // System.out.println(count);
+      //    System.out.println(x++);
 
+      // }
+
+      toadies();
+   }
+
+   public static void toadies() {
+
+      int x = 10;
+
+      switch (x) {
+         case 10:
+            x += 15;
+            break;
+         case 12:
+            x -= 5;
+         default:
+            break;
       }
+
+      System.out.println(x);
 
    }
 
