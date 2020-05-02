1b3276e85770fbd39bb4eac9df6f3b36c47b0136
Rleaf <calcifer44@gmail.com>
2020-05-02 14:18:32 -0400

    rps.java

---
+++ 220/final/rps.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class rps {
+   
+   public static String userInput;
+   public static String computerInput;
+   public static int computerNumber;
+   
+   public static void main(String[] args) {
+      // Ryan Lin, CSC 120 Final, Rock Paper Scissors Game
+      
+      Scanner input = new Scanner(System.in);
+      Random random = new Random();
+
+      System.out.println("This is a game of rock paper scissors,");
+      
+      System.out.println("Enter 'rock', 'paper', 'scissors'");
+      String userInput = input.nextLine();
+
+      computerNumber = random.nextInt(3) + 1;
+
+      // switch (computerNumber) {
+      //    case 1:
+      //       computerInput = "rock";
+      //       // break;
+      //    case 2:
+      //       computerInput = "paper";
+      //       // break;
+      //    default:
+      //       computerInput = "scissors";
+      //       // break;
+      // }
+
+      if (computerNumber == 1) {
+         computerInput = "rock";
+      } else if (computerNumber == 2) {
+         computerInput = "paper";
+      } else {
+         computerInput = "scissors";
+      }
+
+      System.out.println("The user picked "+ userInput);
+      System.out.println("The computer picked " + computerInput);
+
+      if (userInput.equals("scissors") && computerInput.equals("paper")) {
+         System.out.println("User wins!");
+      } else if (userInput.equals("rock") && computerInput.equals("paper")) {
+         System.out.println("Computer wins!");
+      } else if (userInput.equals("rock") && computerInput.equals("scissors")) {
+         System.out.println("User wins!");
+      } else if (userInput.equals("paper") && computerInput.equals("rock")) {
+         System.out.println("User wins!");
+      } else if (userInput.equals("paper") && computerInput.equals("scissors")) {
+         System.out.println("Computer wins!");
+      } else if (userInput.equals("scissors") && computerInput.equals("rock")) {
+         System.out.println("Computer wins!");
+      } else {
+         System.out.println("The game is a tie!");
+      } 
+
+
+      
+   } 
+}
